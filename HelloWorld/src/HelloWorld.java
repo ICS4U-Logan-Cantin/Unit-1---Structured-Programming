af85d5c4922839c7baec648773c5c72321411a42
Logan Cantin <logan.cantin@gmail.com>
2020-02-05 20:09:41 -0500

    Completed Hello World

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * Created by: Logan Cantin
+ * Created on: 03-Feb-2019
+ * Created for: ICS4U
+ * Day #1 (Hello, World!)
+ * This program concatenates strings and outputs the famous "Hello, World!" string to the console
+ */
+
+public class HelloWorld {
+    public static void main(String args[]){
+
+        // Initialize the first and second strings, concatenate them and then print them
+        String firstString = "Hello, ", secondString = "World!";
+        System.out.println(firstString + secondString);
+
+        // Initialize the third, fourth, and fifth strings, concatenate them and then print them
+        String thirdString = "My ", fourthString = "name is ", fifthString = "Logan.";
+        System.out.println(thirdString + fourthString + fifthString);
+    }
+}
