d03a95eedbfd25898ba51ea53ec9f99a71acf1b3
Logan Cantin <logan.cantin@gmail.com>
2020-02-13 12:50:59 -0500

    Dice Game w/ Comments

---
+++ DiceGame/src/DiceGame.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class DiceGame {
+    private JPanel panel1;
+    private JButton btnGuess;
+    private JLabel lblwrongguesses;
+    private JComboBox comboBox1;
+
+    // Declaring variables
+    private int randomInt;
+    private int numWrong = 0;
+
+    // Get a new random number
+    private void RandomizeNumber(){
+        randomInt = new Random().nextInt(6);
+    }
+
+    // The user's guess was wrong. Update the counter and alert user.
+    private void Wrong() {
+        numWrong++;
+        lblwrongguesses.setText(String.format("Number of wrong guesses: %d", numWrong));
+        JOptionPane.showMessageDialog(null, "Failure.");
+    }
+
+    // The user's guess was right. Reset the counter, generate a new random number, and alert user.
+    private void Right() {
+        RandomizeNumber();
+        numWrong = 0;
+        lblwrongguesses.setText(String.format("Number of wrong guesses: %d", numWrong));
+        JOptionPane.showMessageDialog(null, "Success!");
+    }
+
+    // Constructor
+    public DiceGame(){
+        //Generate new random number
+        RandomizeNumber();
+
+        // When the button gets pressed, evaluate if the guess is correct
+        btnGuess.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if (comboBox1.getSelectedIndex() == randomInt){
+                    Right();
+                }
+                else {
+                    Wrong();
+                }
+            }
+        });
+    }
+
+
+    // Initialize application
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("DiceGame");
+        frame.setContentPane(new DiceGame().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
