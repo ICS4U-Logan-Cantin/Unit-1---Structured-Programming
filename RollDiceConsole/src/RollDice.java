406c372ac86a6d673649b28f55a4661df90600f7
Logan Cantin <logan.cantin@gmail.com>
2020-02-14 12:24:41 -0500

    Finished Roll Dice console

---
+++ RollDiceConsole/src/RollDice.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class RollDice {
+
+    public static void RollDice() {
+        // Prompting the user
+        System.out.print("What is the maximum integer you would like to generate: ");
+
+        // Getting input from the user
+        Scanner input = new Scanner(System.in);
+        String inputString = input.nextLine();
+
+        try {
+            // Getting the int from the input
+            int maxInt = Integer.parseInt(inputString);
+
+            // Making sure that the inputted number is positive
+            if (maxInt < 1) throw new Exception();
+
+            // Generating a number from 1 to maxInt inclusive
+            int random = new Random().nextInt(maxInt) + 1;
+
+            // Printing the answer
+            System.out.printf("The random number is: %d", random);
+        }
+
+        //Erroneous input
+        catch (Exception e){
+
+            // Alert user and restart.
+            System.out.println("Incorrect input, you must input a positive integer.");
+            RollDice();
+        }
+    }
+
+    public static void main(String[] args) {
+        RollDice();
+    }
+}
